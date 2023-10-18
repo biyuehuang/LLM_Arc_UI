f8ace9300aa89d6e6a7a9f11d04649df843bd33b
biyuehuang <31006098+biyuehuang@users.noreply.github.com>
2023-10-18 10:37:42 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,5 +4,6 @@ export http_proxy=""
 export https_proxy=""
 #export USE_XETLA=OFF
 #export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1
+
 #conda install -c conda-forge -y gperftools=2.10
 #export LD_PRELOAD=${LD_PRELOAD}:/home/a770/miniconda3/envs/llm2/lib/libtcmalloc.so
